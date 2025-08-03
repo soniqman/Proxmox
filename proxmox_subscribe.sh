68b0583796d0cec1c4a001abd578beeeea6d3317
soniqman <81860678+soniqman@users.noreply.github.com>
2025-08-03 20:10:08 +0300

    proxmox_subscribe.sh

--- proxmox_subscribe.sh
+++ proxmox_subscribe.sh
@@ -2,17 +2,17 @@
 
 echo "▶ Запуск скрипта..."
 
-cd /usr/share/javascript/proxmox-widget-toolkit || {
+cd /usr/share/javascript/proxmox-widget-toolkit/ || {
   echo "❌ Каталог не найден"
   exit 1
 }
 
-cp proxmox.js proxmox.js.back || {
+cp proxmoxlib.js proxmoxlib.js.back || {
   echo "❌ Ошибка при создании резервной копии"
   exit 1
 }
 
-sed 's/res\.data\.status\.toLowerCase() !== '\''active'\''/res.data.status.toLowerCase() == '\''active'\''/g' proxmox.js > proxmox.tmp && mv proxmox.tmp proxmox.js || {
+sed 's/res\.data\.status\.toLowerCase() !== '\''active'\''/res.data.status.toLowerCase() == '\''active'\''/g' proxmoxlib.js > proxmoxlib.tmp && mv proxmoxlib.tmp proxmoxlib.js || {
   echo "❌ Ошибка при изменении файла"
   exit 1
 }
